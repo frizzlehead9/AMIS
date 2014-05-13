9d931b3dbe18f26c0c7b010d965aa1e4d0a5ad90
jonathan.thompson <jonathan.thompson@issinc.com>
2014-05-12 18:58:15 -0600

    added configure screen

---
+++ AMIS/src/com/issinc/amis/activities/ConfigureActivity.java
@@ -0,0 +1,15 @@
+package com.issinc.amis.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.example.amis.R;
+
+/**
+ * Created by jonathan.thompson on 5/12/2014.
+ */
+public class ConfigureActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.configure);
+    }
+}
--- AMIS/src/com/issinc/amis/activities/MainActivity.java
+++ AMIS/src/com/issinc/amis/activities/MainActivity.java
@@ -1,7 +1,9 @@
-package com.issinc.amis;
+package com.issinc.amis.activities;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import com.example.amis.R;
 
 public class MainActivity extends Activity {
@@ -12,5 +14,14 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+
+    /***
+     * Opens the configure view
+     * @param view
+     */
+    public void configure(View view) {
+        Intent intent = new Intent(this, ConfigureActivity.class);
+        startActivity(intent);
     }
 }
