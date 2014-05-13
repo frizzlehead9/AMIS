72c0975ff1fcac2d5d56303747eaa059d6070e98
jonathan.thompson <jonathan.thompson@issinc.com>
2014-05-12 18:27:11 -0600

    added main screen

--- AMIS/src/com/issinc/amis/MainActivity.java
+++ AMIS/src/com/issinc/amis/MainActivity.java
@@ -1,7 +1,8 @@
-package com.example.amis;
+package com.issinc.amis;
 
 import android.app.Activity;
 import android.os.Bundle;
+import com.example.amis.R;
 
 public class MainActivity extends Activity {
     /**
